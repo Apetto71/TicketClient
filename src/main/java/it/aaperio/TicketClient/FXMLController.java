31a05bb9a063f31b3d69a3e741c7b4294d8a9f4a
aaperio <aaperio@aaperio-HP-Pavilion-Desktop-595-p0xxx>
2020-04-29 18:18:59 +0200

    Preparazione del FXML Controller Scheletro della funziona di connessione

--- src/main/java/it/aaperio/TicketClient/FXMLController.java
+++ src/main/java/it/aaperio/TicketClient/FXMLController.java
@@ -1,42 +1,74 @@
-package it.aaperio.TicketClient;
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
 
+package it.aaperio.TicketClient;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.apache.log4j.Logger;
+
 import it.aaperio.TicketClient.Model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
+
+	private Model model ;
+    private Logger logger ; 
 	
-	private Model model; 
 	
-    @FXML // ResourceBundle that was given to the FXMLLoader
+	@FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // fx:id="txtserver"
-    private TextField txtserver; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtServer;
 
-    @FXML // fx:id="txtuser"
-    private TextField txtuser; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtUser;
 
-    @FXML // fx:id="txtpassword"
-    private PasswordField txtpassword; // Value injected by FXMLLoader
+    @FXML
+    private PasswordField txtPassword;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private Button btnConnetti;
+
+    @FXML
+    void hdlConnetti(ActionEvent event) {
+    	
+    	// Verifico che i campi siano stati inseriti
+    	
+    	
+    	// Richiamo il model per l'invio della connessione. Attendo un valore booleano di risposta
+    	// Se la risposta è true sono connesso: 
+    					// disabilito la parte di interfaccia di connessione
+    					// abilito il campo disconnect
+    					// Imposto i nuovi  valori di default nel file di configurazione
+    	// Altrimenti apro un popup per avvisare che non è andata a buon fine.
+    }
+
+    @FXML
     void initialize() {
-        assert txtserver != null : "fx:id=\"txtserver\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtuser != null : "fx:id=\"txtuser\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtpassword != null : "fx:id=\"txtpassword\" was not injected: check your FXML file 'Scene.fxml'.";
-
+        assert txtServer != null : "fx:id=\"txtServer\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtUser != null : "fx:id=\"txtUser\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtPassword != null : "fx:id=\"txtPassword\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnConnetti != null : "fx:id=\"btnConnetti\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+        // Imposto i campi di default
+        logger.debug ("Imposto il valore di default del Server") ;
+        txtServer.setText(model.getConfig().getHOST()) ;
+        txtUser.setText (model.getConfig().getLAST_USER()) ;
     }
     
     public void setModel(Model model) {
-    	this.model = model;
+    	this.model = Model.getModel();
     }
 }
+
