ea7db23ba9c3fcd6ce4c2c8a17ad197c0f7b3f8e
aaperio <aaperio@aaperio-HP-Pavilion-Desktop-595-p0xxx>
2020-04-29 16:31:39 +0200

    Sistemazione del Model/View Pattern. Modificato Entrypoint per creare il Model e settarlo nel FXMLController

--- src/main/java/it/aaperio/TicketClient/FXMLController.java
+++ src/main/java/it/aaperio/TicketClient/FXMLController.java
@@ -1,33 +1,42 @@
 package it.aaperio.TicketClient;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.aaperio.TicketClient.Model.Model;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
-
-    @FXML
+	
+	private Model model; 
+	
+    @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
-    @FXML
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML
-    private TextField txtserver;
+    @FXML // fx:id="txtserver"
+    private TextField txtserver; // Value injected by FXMLLoader
 
-    @FXML
-    private TextField txtuser;
+    @FXML // fx:id="txtuser"
+    private TextField txtuser; // Value injected by FXMLLoader
 
-    @FXML
-    private PasswordField txtpassword;
+    @FXML // fx:id="txtpassword"
+    private PasswordField txtpassword; // Value injected by FXMLLoader
 
-    @FXML
+    @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert txtserver != null : "fx:id=\"txtserver\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtuser != null : "fx:id=\"txtuser\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtpassword != null : "fx:id=\"txtpassword\" was not injected: check your FXML file 'Scene.fxml'.";
 
+    }
+    
+    public void setModel(Model model) {
+    	this.model = model;
     }
 }
